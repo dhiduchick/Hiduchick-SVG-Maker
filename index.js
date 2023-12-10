1183f1a2be16df4ab512c69f0b5b1aa4e3a5c1f9
Daelyn Hiduchick <DHIDUCHICK@GMAIL.COM>
2023-12-10 15:10:13 -0500

    inquirer,fs, and classes from shapes.js added to index.html

---
+++ index.js
@@ -0,0 +1,9 @@
+// Inquirer (node package manager) import
+const inquirer = require('inquirer');
+
+//file system module import
+const fs = require('fs');
+
+// importing classes fomr ./lib/shapes directory
+const {Triangle, Square, Circle } = require('./lib/shapes')
+
